91e8ae841853edd139b07991f33460199b5f62ff
Florian Ulmschneider <florian@ulms.ch>
2017-01-03 17:21:59 +0100

    Moved emulator functions to separate file

--- emulator/main.c
+++ emulator/main.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 //#include <iostream>
 //#include <vector>
-#include <stdlib.h>
 #include <ncurses.h>
+//#include <errno.h>
+//#include <string.h>
+#include <termios.h>
+#include <pty.h>
+#include <stdlib.h>
 #include <poll.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include <termios.h>
-#include <pty.h>
-
 #include <z80.h>
 
 
+
+
 #include "main.h"
-//#include "module.h"
+#include "emulator.h"
 #include "sd.h"
 
-extern int errno;
 
-struct termios ptyTermios;
-
-FILE *memFile;
-byte memory[0x10000];
-static Z80Context context;
-struct SdCard sd;
-struct SdModule sdModule;
 
 /*
 Module* modules[8] = {
@@ -41,7 +34,6 @@ Module* modules[8] = {
 };
 */
 
-struct pollfd pty[1];
 
 int main(int argc, char **argv) {
 	int hflag = 0;
@@ -81,33 +73,6 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	memFile = fopen(romFile, "rb");
-	fread(memory, 1, 0x10000, memFile);
-	fclose(memFile);
-	int ptm, pts;
-	char *ptsName;
-	ptsName = (char*) malloc(50);
-	int error = openpty(&ptm, &pts, ptsName, NULL, NULL);
-	tcgetattr(ptm, &ptyTermios);
-	cfmakeraw(&ptyTermios);
-	tcsetattr(ptm, TCSANOW, &ptyTermios);
-
-
-	symlink(ptsName, "/tmp/zi28sim");
-	free(ptsName);
-
-
-	if (ptm < 0){
-		printf("error");
-		fprintf(stderr, "Value of errno: %d\n", errno);
-		fprintf(stderr, "Error opening file: %s\n", strerror(errno));
-		return 2;}
-	pty[0].fd = ptm;
-	pty[0].events = POLLIN;
-
-	sd.imgFile = fopen("/home/florian/sd.img", "rb");
-	sd.status = IDLE;
-	sdModule.card = &sd;
 
 
 	//Start curses
@@ -118,7 +83,8 @@ int main(int argc, char **argv) {
 	nodelay(stdscr, true);
 
 	//Start z80lib
-	init_emulator();
+	emulator_init();
+	emulator_loadRom(romFile);
 
 	while (1) {
 		char c = getch();
@@ -138,67 +104,4 @@ int main(int argc, char **argv) {
 
 	}
 	return 1;
-}
-
-static byte context_mem_read_callback(int param, ushort address) {
-	return memory[address];
-}
-
-static void context_mem_write_callback(int param, ushort address, byte data) {
-	memory[address] = data;
-}
-
-static byte context_io_read_callback(int param, ushort address) {
-	int data=0xff;
-	int ret;
-	address = address & 0xff;
-
-	if (address >= 0x80) {
-		int base = (address - 0x80) / 0x10;
-		ushort offs = (address - 0x80) % 0x10;
-		data = SdModule_read(&sdModule, offs);
-
-	} else {
-		switch (address) {
-			case 0x00:
-				read(pty[0].fd, &data, 1);
-				break;
-			case 0x01:
-				data = 0x02;
-				ret = poll(pty, 1, 0);
-				if ((ret > 0) && (pty[0].revents & POLLIN)) {
-					data = 0x00;
-				}
-				break;
-			default:
-				break;
-		}
-	}
-
-	return data;
-}
-
-static void context_io_write_callback(int param, ushort address, byte data) {
-	address = address & 0xff; // port address
-	if (address >= 0x80) {
-		int base = (address - 0x80) / 0x10;
-		ushort offs = (address - 0x80) % 0x10;
-		SdModule_write(&sdModule, offs, data);
-
-	} else {
-		switch (address) {
-			case 0x00:
-				if (data != 0x0D) write(pty[0].fd, &data, 1);
-				break;
-			default:
-				break;
-		}
-	}
-}
-
-void init_emulator() {
-	context.memRead = context_mem_read_callback;
-	context.memWrite = context_mem_write_callback;
-	context.ioRead = context_io_read_callback;
-	context.ioWrite = context_io_write_callback;
 }
