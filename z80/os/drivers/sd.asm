31c7f0b430a24a92eb20b070b5380caad690f846
Florian Ulmschneider <florian@ulms.ch>
2017-02-17 17:59:01 +0100

    SD-Card driver outline

--- z80/os/drivers/sd.asm
+++ z80/os/drivers/sd.asm
@@ -1,5 +1,24 @@
 .list
-sd_read:
-sd_write:
-sd_seek:
-sd_ctl:
+
+; SD-Card driver
+
+sd_fileDriver:
+	.dw sd_read
+	.dw sd_write
+;	.dw sd_seek
+
+
+.func sd_read:
+;; Inputs: ix = file entry addr, (de) = buffer, bc = count
+;; a = errno, de = count
+;; Errors: 0=no error
+
+.endf ;sd_read
+
+
+.func sd_write:
+;; Inputs: ix = file entry addr, (de) = buffer, bc = count
+;; a = errno, de = count
+;; Errors: 0=no error
+
+.endf ;sd_write
