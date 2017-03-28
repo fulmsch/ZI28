7a5a0bb38f3f136f310d1e5afc61e3e883caee82
Florian Ulmschneider <florian@ulms.ch>
2017-03-28 08:58:41 +0200

    SDC-Driver basic structure

--- z80/os/src/drivers/sd.asm
+++ z80/os/src/drivers/sd.asm
@@ -1,24 +1,59 @@
+;; SD-Card driver
 .list
-
-; SD-Card driver
 
 sd_fileDriver:
 	.dw sd_read
 	.dw sd_write
-;	.dw sd_seek
+
+.define SD_ENABLE out (82h), a
+.define SD_DISABLE out (83h), a
 
 
 .func sd_read:
-;; Inputs: ix = file entry addr, (de) = buffer, bc = count
-;; a = errno, de = count
-;; Errors: 0=no error
+;; Inputs
+;; : ix - file entry addr
+;; : (de) - buffer
+;; : bc - count
+;;
+;; Output:
+;; : de = count
+;; : a - errno
+
+; Errors: 0=no error
+
+	ret
 
 .endf ;sd_read
 
 
 .func sd_write:
-;; Inputs: ix = file entry addr, (de) = buffer, bc = count
-;; a = errno, de = count
-;; Errors: 0=no error
+;; Input:
+;; : ix - file entry addr
+;; : (de) - buffer
+;; : bc - count
+;;
+;; Output:
+;; : de - count
+;; : a - errno
+
+; Errors: 0=no error
+
+	ret
 
 .endf ;sd_write
+
+
+.func delay100:
+	;Wait for approx. 100ms
+	ld b, 0
+	ld c, 41
+loop:
+	ex (sp), hl
+	ex (sp), hl
+	ex (sp), hl
+	ex (sp), hl
+	djnz delay100Loop
+	dec c
+	jr nz, delay100Loop
+	ret
+.endf ;delay100
