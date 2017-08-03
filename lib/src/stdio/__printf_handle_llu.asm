4ef2c094b43faeb49de780bbea3ea20a635bd5ec
Florian Ulmschneider <florian@ulms.ch>
2017-08-03 17:33:43 +0200

    Made printf work

--- lib/src/stdio/__printf_handle_llu.asm
+++
@@ -1,9 +0,0 @@
-
-	MODULE	__printf_handle_llu
-	SECTION code_clib
-	PUBLIC	__printf_handle_llu
-	EXTERN	__printf_number64
-
-__printf_handle_llu:
-	ld	c,0		;unsigned
-	jp	__printf_number64
--- lib/src/stdio/stdstreams.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-void stdstreams() {
-	// Set up stdin, stdout and stderr
-	fdopen(STDIN_FILENO, "r");
-	fdopen(STDOUT_FILENO, "a");
-	fdopen(STDERR_FILENO, "a");
-}
