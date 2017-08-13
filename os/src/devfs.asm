3297730dcefd64982d915360baff6f7e49cb4c0a
Florian Ulmschneider <florian@ulms.ch>
2017-08-13 16:30:49 +0200

    Add readdir and fstat to devfs

--- os/src/devfs.asm
+++ os/src/devfs.asm
@@ -15,8 +15,13 @@ devfs_fsDriver:
 	.dw devfs_readdir
 	.dw devfs_fstat
 
-.define dev_fileTableNumber fileTableData
-.define dev_fileTableData   dev_fileTableNumber + 1
+.define dev_fileTableDirEntry fileTableData             ;Pointer to entry in devfs
+.define dev_fileTableNumber   dev_fileTableDirEntry + 2
+.define dev_fileTableData     dev_fileTableNumber + 1
+
+devfs_fileDriver:
+	.dw 0x0000 ;devfs_read
+	.dw 0x0000 ;devfs_write
 
 
 .func devfs_init:
@@ -155,7 +160,6 @@ error:
 ;; Input:
 ;; : ix - table entry
 ;; : (de) - absolute path
-;; : a - mode
 ;;
 ;; Output:
 ;; : a - errno
@@ -163,6 +167,39 @@ error:
 ; Errors: 0=no error
 ;         4=no matching file found
 
+	ld a, (de)
+	cp 0x00
+	jr nz, notRootDir
+	;root directory
+
+	;store file driver
+	ld a, devfs_fileDriver & 0xff
+	ld (ix + fileTableDriver), a
+	ld a, devfs_fileDriver >> 8
+	ld (ix + fileTableDriver + 1), a
+
+	;store size
+	ld de, devfsEntries * devfsEntrySize
+	ld b, ixh
+	ld c, iyl
+	ld hl, fileTableSize
+	add hl, bc
+	call ld16
+
+	;set type to directory
+	ld a, (ix + fileTableMode)
+	or M_DIR
+	ld (ix + fileTableMode), a
+
+	;set dirEntry pointer to 0 to indicate root dir
+	xor a
+	ld (ix + dev_fileTableDirEntry), a
+	ld (ix + dev_fileTableDirEntry + 1), a
+
+	ret
+
+
+notRootDir:
 	ld hl, devfsRoot
 	push de ;path
 	push hl ;file entry
@@ -206,6 +243,9 @@ fileFound:
 	push hl
 	ld d, iyh
 	ld e, iyl
+	;store dirEntry pointer while we have a pointer in a register
+	ld (ix + dev_fileTableDirEntry), e
+	ld (ix + dev_fileTableDirEntry + 1), d
 	ld hl, devfs_data
 	add hl, de
 	pop de
@@ -233,12 +273,116 @@ invalidFile:
 
 
 .func devfs_readdir:
+;; Get information about the next file in a directory.
+;;
+;; Input:
+;; : a - dirfd
+;; : (de) - stat
+;;
+;; Output:
+;; : a - errno
 
+	push af
+
+	;check if root dir
+	ld a, (ix + dev_fileTableDirEntry)
+	cp 0x00
+	jr nz, error
+	ld a, (ix + dev_fileTableDirEntry + 1)
+	cp 0x00
+	jr nz, error
+
+	ld c, (ix + fileTableOffset)
+	ld b, (ix + fileTableOffset + 1)
+	ld hl, devfsRoot
+	add hl, bc
+
+	xor a
+	cp (hl)
+	jr z, error ;end of dir
+
+	;seek to next entry
+	pop af
+	push de
+	push hl
+	ld de, devfsEntrySize
+	ld h, K_SEEK_PCUR
+	call k_seek
+	pop hl
+	pop de
+
+	;hl points to dirEntry
+	jr devfs_statFromEntry
+
+
+error:
+	pop af
+	ld a, 1
 	ret
 .endf
 
 
 .func devfs_fstat:
+;; Get information about a file.
+;;
+;; Input:
+;; : ix - file entry addr
+;; : (de) - stat
+;;
+;; Output:
+;; : a - errno
 
+
+	;check if root dir
+	ld a, (ix + dev_fileTableDirEntry)
+	cp 0x00
+	jr nz, notRootDir
+	ld a, (ix + dev_fileTableDirEntry + 1)
+	cp 0x00
+	jr z, rootDir
+
+notRootDir:
+	ld b, ixh
+	ld c, ixl
+	ld hl, dev_fileTableDirEntry
+	add hl, bc
+	;hl points to dirEntry
+	jr devfs_statFromEntry
+
+rootDir:
+	xor a
+	ld (de), a ;name = null
+	ld hl, STAT_ATTRIB
+	add hl, de
+	;TODO permission of drive
+	ld (hl), SP_READ | SP_WRITE | ST_DIR
+	;file size is unspecified
+	;a = 0
+	ret
+.endf
+
+
+.func devfs_statFromEntry:
+;; Creates a stat from a directory entry.
+;;
+;; Input:
+;; : (hl) - dir entry
+;; : (de) - stat
+
+	;copy name
+	push de
+	call strcpy
+	pop de
+	ex de, hl
+	;(hl) = stat, (de) = dirEntry
+	ld bc, STAT_ATTRIB
+	add hl, bc
+	;(hl) = stat_attrib
+	;TODO store actual attribs
+	ld (hl), SP_READ | SP_WRITE | ST_CHAR
+
+	;file size is unspecified
+
+	xor a
 	ret
 .endf
