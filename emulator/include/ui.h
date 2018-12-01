4fa189bdcd57927876640296c15c0505aa570e8a
Florian Ulmschneider <florian@ulms.ch>
2018-12-01 11:42:16 +0100

    Add printInstruction.

--- emulator/include/ui.h
+++ emulator/include/ui.h
@@ -1,6 +1,9 @@
 #ifndef UI_H
 #define UI_H
 
+#include "libz80/z80.h"
+
 void console(const char* format, ...);
+void printInstruction(Z80Context *context);
 
 #endif
--- emulator/src/ui.c
+++ emulator/src/ui.c
@@ -3,27 +3,21 @@
 #include <stdarg.h>
 #include <readline/readline.h>
 
+#include "ui.h"
+
 void console(const char* format, ...)
 {
 	va_list args;
-	char buffer[100];
 
 	va_start(args, format);
-	vsnprintf(buffer, 100, format, args);
+	vprintf(format, args);
 	va_end(args);
+}
 
-//	// Readline magic from https://stackoverflow.com/a/15541914
-//	int saved_point = rl_point;
-//	char *saved_line = rl_copy_text(0, rl_end);
-//	rl_save_prompt();
-//	rl_replace_line("", 0);
-//	rl_redisplay();
 
-	printf("%s", buffer);
-
-//	rl_restore_prompt();
-//	rl_replace_line(saved_line, 0);
-//	rl_point = saved_point;
-//	rl_redisplay();
-//	free(saved_line);
+void printInstruction(Z80Context *context)
+{
+	char str[20];
+	Z80Debug(context, NULL, str);
+	console("0x%04X  %s\n", context->PC, str);
 }
