ef4bdbabd6b301c9519185dcb77a8110d120dd8a
Florian Ulmschneider <florian@ulms.ch>
2019-06-16 10:21:53 +0200

    Add font table and primitive text rendering routine.

--- system/os/src/shell/builtins/test.asm
+++ system/os/src/shell/builtins/test.asm
@@ -1,7 +1,7 @@
 SECTION rom_code
 INCLUDE "cli.h"
 
-EXTERN k_unlink
+EXTERN fontTable
 
 PUBLIC b_test
 b_test:
@@ -16,5 +16,75 @@ b_test:
 	ld e, (hl)
 	inc hl
 	ld d, (hl)
-	;(de) = path name
-	jp k_unlink
+	;(de) = string
+
+	xor a
+	ld (xOffs), a
+	ld (yOffs), a
+	out (0x93), a
+
+loop:
+	ld a, (de)
+	cp 0
+	ret z
+
+	push de
+
+	ld hl, fontTable
+	ld c, a
+	ld b, 0
+
+	sla c
+	rl b
+	sla c
+	rl b
+	sla c
+	rl b
+
+	adc hl, bc
+	;(hl): font table entry
+	ld b, 8
+
+vLoop:
+	ld a, 8
+	sub a, b
+	out (0x92), a ;yreg
+	ld a, (xOffs)
+	out (0x91), a ;xreg
+
+	ld d, (hl)
+	inc hl
+
+	ld c, 3
+hloop:
+	ld a, 0 ;set both pixels to black
+	rlc d
+	jr nc, blank0
+	or 0x0f
+blank0:
+	rlc d
+	jr nc, blank1
+	or 0xf0
+blank1:
+	out (0x97), a ;data inc
+
+	dec c
+	jr nz, hloop
+
+
+
+	djnz vLoop
+
+
+	pop de
+	inc de
+	ld hl, xOffs
+	ld a, 3
+	add a, (hl)
+	ld (hl), a
+	jr loop
+	ret
+
+SECTION ram_os
+xOffs: defb 0
+yOffs: defb 0
